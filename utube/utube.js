3d9d8d54c95055fd9fe79a5664bd50c981e5b191
Akbarali-Turgunov <148555069+Akbarali-Turgunov@users.noreply.github.com>
2023-10-21 18:13:56 +0500

    Add files via upload

---
+++ utube/utube.js
@@ -0,0 +1,189 @@
+const data = [
+    {
+        "id": 1,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "James Gouse",
+        "wiews": "15K",
+        "when": "1 week ago",
+        "user": "https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url": "https://avatars.mds.yandex.net/i?id=e2f14a1369795bf52b4f7d5652c1acdeebf44610-8496961-images-thumbs&n=13"
+
+    },
+    {
+        "id": 2,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "Alan Cooper",
+        "wiews": "18K",
+        "when": "1 week ago",
+        "user": "https://www.youtube.com/watch?v=aTHBJwVgu3I",
+        "url": "https://avatars.mds.yandex.net/i?id=10b9d356f46dac1313de0ac8022f8e47b30e7751-9145401-images-thumbs&n=13"
+
+    },
+    {
+        "id": 3,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "Marcus Levin",
+        "wiews": "15K",
+        "when": "Alexis Sears",
+        "user": "https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url": "https://avatars.mds.yandex.net/i?id=3f5ad4cccc90d9bba8fe27c96ecae53b3737a2c6-9311909-images-thumbs&n=13"
+
+    },
+    {
+        "id": 4,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "James Gouse",
+        "wiews": "20K",
+        "when": "1 week ago",
+        "user": "https://www.youtube.com/watch?v=SjDFJCCB6bs&t=827s",
+        "url": "https://avatars.mds.yandex.net/i?id=346c98e80343b3c8bb0ca0d93dc0ca8fc237149c-9559767-images-thumbs&n=13"
+
+    },
+    {
+        "id": 5,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "Jesica Lambert",
+        "wiews": "11K",
+        "when": "1 week ago",
+        "user": "https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url": "https://www.youtube.com/watch?v=9n2JMn66MRw"
+
+    },
+    {
+        "id": 6,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "Anna White",
+        "wiews": "9K",
+        "when": "1 week ago",
+        "user": "https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url": "https://www.youtube.com/watch?v=6XcmZBiw9C8"
+
+    },
+    {
+        "id": 7,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "Skylar Dias",
+        "wiews": "1K",
+        "when": "1 week ago",
+        "user": "https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url": "https://www.youtube.com/watch?v=Xd0M8LTVRZ0"
+
+    },
+    {
+        "id": 8,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "James Gouse",
+        "wiews": "3K",
+        "when": "1 week ago",
+        "user": "https://www.youtube.com/watch?v=M5QY2_8704o&t=4725s&pp=ygUMY29kaW5nIG11c2lj",
+        "url": "https://www.youtube.com/watch?v=t1lp9p-TC18"
+
+    },
+    {
+        "id": 9,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "James Gouse",
+        "wiews": "15K",
+        "when": "1 week ago",
+        "user": "https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url": "https://www.youtube.com/watch?v=ZtDYsqAVPbI"
+
+    },
+    {
+        "id": 10,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "James Gouse",
+        "wiews": "2K",
+        "when": "1 week ago",
+        "user": "https://www.youtube.com/watch?v=1LH5JcJtV94&t=1702s&pp=ygUMY29kaW5nIG11c2lj",
+        "url": "https://www.youtube.com/watch?v=uEIpmfe5qb4"
+
+    },
+    {
+        "id": 11,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "James Gouse",
+        "wiews": "15K",
+        "when": "1 week ago",
+        "user": "https://www.youtube.com/watch?v=W0VpO8ZaqNs&t=2317s&pp=ygUMY29kaW5nIG11c2lj",
+        "url": "https://youtu.be/Fzu4vkgb5N0?t=2"
+    },
+    {
+        "id": 12,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "James Gouse",
+        "wiews": "8K",
+        "when": "1 week ago",
+        "user": "https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url": "https://youtu.be/xFXmP89-JY8?t=2"
+
+    },
+    {
+        "id": 13,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "James Gouse",
+        "wiews": "6K",
+        "when": "1 week ago",
+        "user": "https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url": "https://www.youtube.com/shorts/mXli6p_y-Ok?t=7&feature=share"
+
+    },
+    {
+        "id": 14,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "James Gouse",
+        "wiews": "17K",
+        "when": "1 week ago",
+        "user": "https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url": "https://www.youtube.com/shorts/_EIfr-xsgVs?t=7&feature=share"
+
+    },
+    {
+        "id": 14,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "James Gouse",
+        "wiews": "23K",
+        "when": "1 week ago",
+        "user": "https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url": "https://www.youtube.com/shorts/1sNt3FBtLq4?t=7&feature=share"
+
+    },
+    {
+        "id": 15,
+        "title": "Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name": "James Gouse",
+        "wiews": "19K",
+        "when": "1 week ago",
+        "user": "https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url": "https://youtu.be/qIqAFibHQXg?t=7"
+
+    }
+]
+
+const soft = document.querySelector(".soft")
+
+data.forEach(element => {
+    soft.innerHTML += `
+    <div class="soft__card">
+    
+                    <a href="${element.url}" target="_blank"><img src="${element.url}" class="soft__rasm"></a>
+                    <div class="soft__frond">
+                        <div>
+                            <img src="${element.user}" alt="">
+                        </div>
+                        <div class="soft__right">
+                            <span class="soft__time"></span>
+                            <h2 class="soft__title">${element.title}</h2>
+                            <p class="soft__text">${element.name}</p>
+                            <span class="soft__views">${element.wiews} Views .</span>
+                            <span class="soft__when">${element.when} ago</span>
+                        </div>
+                    </div>
+    </div>`
+});
+
+const v = document.querySelector(".navbar__rasm");
+const z=document.querySelector(".sidebar");
+v.addEventListener("click",function(){
+    v.classList.toggle("aktiv")
+})
